07600eabca5504c17d697f7e5134817ba9364fc7
Diego <dcayo.156@gmail.com>
2020-02-04 16:18:37 -0400

    Cambios DIego 04-02-20
    Venta de servicios

--- DinoM/P_Principal.vb
+++ DinoM/P_Principal.vb
@@ -1025,6 +1025,22 @@ Public Class P_Principal
         tab3.Icon = frm.Icon
     End Sub
 
+    Private Sub btVentaServicio_Click(sender As Object, e As EventArgs) Handles btVentaServicio.Click
+        SideNav1.IsMenuExpanded = False
+        Ventana.Select()
+        Dim frm As New F0_VentasServicio
+        frm._nameButton = btVentaServicio.Name
+        Dim tab3 As SuperTabItem = superTabControl3.CreateTab(frm.Text)
+        frm._tab = tab3
+        frm._modulo = FP_VENTAS
+        Dim panel As Panel = P_Global._fnCrearPanelVentanas(frm)
+        superTabControl3.SelectedTabIndex = superTabControl3.Tabs.Count - 1
+        tab3.AttachedControl.Controls.Add(panel)
+        frm.Show()
+        tab3.Text = frm.Text
+        tab3.Icon = frm.Icon
+    End Sub
+
     'Private Sub btnCredPagoCliente_Click(sender As Object, e As EventArgs) Handles btnCredPagoCliente.Click
     '    SideNav1.IsMenuExpanded = False
     '    Ventana.Select()
